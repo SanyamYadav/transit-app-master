c829f10be814f600427f25e552a448ebb2360de5
SanyamYadav <samsanyam16@yahoo.co.in>
2018-01-12 11:09:29 +0000

    Update TransitAppError.swift

--- TransitApp/TransitApp/TransitAppError.swift
+++ TransitApp/TransitApp/TransitAppError.swift
@@ -2,10 +2,6 @@
 //  ErrorHelper.swift
 //  TransitApp
 //
-//  Created by Julio Cesar Fausto on 24/02/16.
-//  Copyright © 2016 Julio Cesar Fausto. All rights reserved.
-//
-
 import Foundation
 
 enum TransitAppError: ErrorType {
