cc63583ba35412bf11b62877b8c9180de4887ef2
SanyamYadav <samsanyam16@yahoo.co.in>
2018-01-12 11:10:23 +0000

    Update UIColor+Extension.swift

--- TransitApp/TransitApp/UIColor+Extension.swift
+++ TransitApp/TransitApp/UIColor+Extension.swift
@@ -2,8 +2,6 @@
 //  UIColor+Extension.swift
 //  TransitApp
 //
-//  Created by Julio Cesar Fausto on 24/02/16.
-//  Copyright © 2016 Julio Cesar Fausto. All rights reserved.
 //
 
 import Foundation
