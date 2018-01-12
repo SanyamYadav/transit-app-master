025f69fccd80a7bc883fb246c5e4e6b2d17a49e3
SanyamYadav <samsanyam16@yahoo.co.in>
2018-01-12 14:52:55 +0000

    Update DataProvider.swift

--- TransitApp/TransitApp/DataProvider.swift
+++ TransitApp/TransitApp/DataProvider.swift
@@ -2,10 +2,6 @@
 //  DataProvider.swift
 //  TransitApp
 //
-//  Created by Julio Cesar Fausto on 28/02/16.
-//  Copyright © 2016 Julio Cesar Fausto. All rights reserved.
-//
-
 import Foundation
 import Unbox
 
