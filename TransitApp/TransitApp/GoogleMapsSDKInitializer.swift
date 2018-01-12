f022e20e794de2012c025f4df0b32b85712832b1
SanyamYadav <samsanyam16@yahoo.co.in>
2018-01-12 01:25:27 +0000

    Update GoogleMapsSDKInitializer.swift

--- TransitApp/TransitApp/GoogleMapsSDKInitializer.swift
+++ TransitApp/TransitApp/GoogleMapsSDKInitializer.swift
@@ -2,10 +2,6 @@
 //  GoogleMapsSDKInitializer.swift
 //  TransitApp
 //
-//  Created by Julio Cesar Fausto on 26/02/16.
-//  Copyright © 2016 Julio Cesar Fausto. All rights reserved.
-//
-
 import Foundation
 import GoogleMaps
 
