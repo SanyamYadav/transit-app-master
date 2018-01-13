5f51925912751092f2369bebe1e39040a68928e4
SanyamYadav <samsanyam16@yahoo.co.in>
2018-01-13 12:02:26 +0000

    Update DateFormatterHelper.swift

--- TransitApp/TransitApp/DateFormatterHelper.swift
+++ TransitApp/TransitApp/DateFormatterHelper.swift
@@ -13,6 +13,7 @@ class DateFormatter {
     let formatterForJsonConversion: NSDateFormatter
     
     init() {
+        
         self.formatterForJsonConversion = NSDateFormatter()
         self.formatterForJsonConversion.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZZZZZ"
         self.formatterForJsonConversion.timeZone = NSTimeZone(forSecondsFromGMT: 0)
